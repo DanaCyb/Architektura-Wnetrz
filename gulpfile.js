8f3372b39f3c7901a8efe0c61063eaf149baee0b
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2022-11-09 16:56:18 +0100

    Add task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass')(require('sass'));
 
 var cssDest = 'style';
 var cssInputFile = 'source/style.scss';
+var watchedFiles = 'source/**/*.scss';
 
 gulp.task('buildcss', function(){
   return gulp.src(cssInputFile)
@@ -10,4 +11,8 @@ gulp.task('buildcss', function(){
     outputStyle: 'compressed'
   }))
   .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(watchedFiles, ['buildcss']);
 });
