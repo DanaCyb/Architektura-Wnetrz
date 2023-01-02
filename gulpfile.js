e3b16d8f7785f14cb3d0f49c3092d87924de7080
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2023-01-02 11:26:25 +0100

    Mod gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+const {src, dest, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssDest = 'style';
+const cssInputFile = 'source/style.scss';
 
-var cssDest = 'style';
-var cssInputFile = 'source/style.scss';
-var watchedFiles = 'source/**/*.scss';
-
-gulp.task('buildcss', function(){
-  return gulp.src(cssInputFile)
-  .pipe(sass({
-    outputStyle: 'compressed'
-  }))
-  .pipe(gulp.dest(cssDest));
-});
-
-gulp.task('watch', function(){
-  gulp.watch(watchedFiles, ['buildcss']);
-});
+function streamSass(){
+  return src(cssInputFile)
+  .pipe(
+      sass({
+          outputStyle: "compressed"
+      }).on('error', sass.logError)
+  )
+  .pipe(dest(cssDest));
+}
